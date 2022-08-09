73d6cbcfe5413bf81ba136260b9fb679c7b9850f
Solrasido55 <106805946+Solrasido55@users.noreply.github.com>
2022-08-09 16:32:06 +0900

    ver1

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const clicks = document.querySelectorAll("li");
+const container = document.querySelectorAll("container");
+const introduction = container[0].offsetTop
+const goal = container[1].offsetTop
+const hobby = container[2].offsetTop
+const cat = container[3].offsetTop
+
+
+clicks[0].addEventListener("click", function(){
+    window.scroll({top: introduction, behavior: 'smooth'})
+})
+
+clicks[1].addEventListener("click", function(){
+    window.scroll({top: goal, behavior: 'smooth'})
+})
+
+clicks[2].addEventListener("click", function(){
+    window.scroll({top: hobby, behavior: 'smooth'})
+})
+
+clicks[3].addEventListener("click", function(){
+    window.scroll({top: cat, behavior: 'smooth'})
+})
